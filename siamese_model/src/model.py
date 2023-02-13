546b41f994d5d5e2564d1878f6717cbdc8e92fcd
Sacha <sacha.lesueur17@gmail.com>
2023-02-13 14:04:34 +0100

    ResNet101 implementation

--- siamese_model/src/model.py
+++ siamese_model/src/model.py
@@ -1,9 +1,26 @@
 import torch
 from torch import nn
-from torchvision.models import resnet50
+from torchvision.models import resnet50, resnet101
 import numpy as np
 
-class SiameseModel(nn.Module):
+class BaseModel(nn.Module):
+    def __init__(self, parallele = False, partial_freeze = False):
+        super().__init__()
+        self.encoder = None
+        pass
+
+    def preprocessing(self, x: np.ndarray):
+        if len(x.shape) == 3:
+            tensor = torch.from_numpy(x).permute(2,0,1)
+        else: 
+            tensor = torch.from_numpy(x).permute(0,3,1,2)
+        return tensor/255
+
+    def forward(self, x):
+        return self.encoder(x)
+
+
+class SiameseModel(BaseModel):
     def __init__(self, parallele = False, partial_freeze = False):
         super().__init__()
         self.encoder = resnet50(weights ='DEFAULT')            
@@ -19,12 +36,18 @@ class SiameseModel(nn.Module):
         if parallele:
             self.encoder = nn.DataParallel(self.encoder)
 
-    def preprocessing(self, x: np.ndarray):
-        if len(x.shape) == 3:
-            tensor = torch.from_numpy(x).permute(2,0,1)
-        else: 
-            tensor = torch.from_numpy(x).permute(0,3,1,2)
-        return tensor/255
+class SMResNet101(BaseModel):
+    def __init__(self, parallele = False, partial_freeze = False):
+        super().__init__()
+        self.encoder = resnet101(weights ='DEFAULT')            
 
-    def forward(self, x):
-        return self.encoder(x)
+        ct = 0
+        if partial_freeze:
+            for child in self.encoder.children():
+                ct += 1
+                if ct < 7:
+                    for param in child.parameters():
+                        param.requires_grad = False
+
+        if parallele:
+            self.encoder = nn.DataParallel(self.encoder)
