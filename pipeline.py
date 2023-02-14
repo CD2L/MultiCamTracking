c4bc00ce94b0c1402f50d5fe047cb835d09bc4a6
Sacha <sacha.lesueur17@gmail.com>
2023-02-14 18:22:58 +0100

    [WIP] pretrained VGG16 implementation

--- pipeline.py
+++ pipeline.py
@@ -2,7 +2,7 @@ import os
 from argparse import ArgumentParser
 import cv2 as cv
 import random
-from siamese_model.src.model import SiameseModel, SMResNet101
+from siamese_model.src.model import SiameseModel, SMResNet101, MCT_VGG16
 from siamese_model.src.utils import distance
 import torch
 from torch import nn
@@ -23,7 +23,7 @@ torch.no_grad()
 def main(opt):
     ref_img = cv.imread(opt['ref'], cv.IMREAD_ANYCOLOR)
     ref_img = cv.cvtColor(ref_img, cv.COLOR_BGR2RGB)
-    ref_img = cv.resize(ref_img, (200,400))
+    ref_img = cv.resize(ref_img, (224,224))
 
     # YoloV8 detection
     os.system(f'python ./yolov8_tracking/track.py --source {opt["image"]} --save-crop --project .temp --vid-stride {opt["stride"]}')
@@ -45,22 +45,28 @@ def main(opt):
         for pth in pimgs:
             im = cv.imread(os.path.join(TEMP_PFOLDER, pfolder_path, pth), cv.IMREAD_ANYCOLOR)
             im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
-            im = cv.resize(im, (200,400))
+            im = cv.resize(im, (224,224))
             img_batch.append(im)
                            
         imgs.append(img_batch)
     
 
     # Similarity model
-    model = SMResNet101()
-    rn101 = nn.DataParallel(model)
-    rn101 = rn101.to('cuda')
+    # model = SMResNet101()
+    # rn101 = nn.DataParallel(model)
+    # rn101 = rn101.to('cuda')
 
-    rn101.load_state_dict(torch.load('./siamese_model/checkpoints-saved/checkpoint_rn101_400.pkl')['model'])
-    
+    # rn101.load_state_dict(torch.load('./siamese_model/checkpoints-saved/checkpoint_rn101_400.pkl')['model'])
+
+    # load the model
+    model = MCT_VGG16()
+
     ref_preprocessed = np.expand_dims(ref_img, 0)
     ref_preprocessed = model.preprocessing(ref_preprocessed)
-    ref_tensor = rn101(ref_preprocessed)
+    print(ref_preprocessed.shape)
+    ref_tensor = model(ref_preprocessed)
+
+    print(ref_tensor)
 
     # Calcul average similarity for each person
     means = []
@@ -70,11 +76,11 @@ def main(opt):
     for  batch_idx, img_batch in enumerate(imgs):
         img_batch = np.array(img_batch, dtype='float32')
         img_batch_preprocessed = model.preprocessing(img_batch)
-        img_batch_tensors = rn101(img_batch_preprocessed)
+        img_batch_tensors = model(img_batch_preprocessed)
 
         distance_res = []
         for img_tensor in img_batch_tensors:
-            distance_res.append(distance(ref_tensor,img_tensor).item())
+            distance_res.append(distance(ref_tensor,img_tensor, type='cosine').item())
             if distance_res[-1] < min_val:
                 min_val = distance_res[-1]
                 min_idx = batch_idx
@@ -83,7 +89,7 @@ def main(opt):
         means.append(np.average(distance_res))
 
 
-    idx = np.argmin(means)
+    idx = np.argmax(means)
 
     print('--------------------')
     print('Folder ID (mean):',idx+1)
@@ -96,12 +102,6 @@ def main(opt):
     cv.imwrite('result-min.jpg', cv.cvtColor(imgs[min_idx][0], cv.COLOR_RGB2BGR))
 
 
-
-        
-
-            
-            
-            
 
 if __name__ == '__main__':
     opt = parser.parse_args()    
