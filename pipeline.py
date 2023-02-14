5d976f2c892ae9fdf4fef76dd02615929f7e114f
Sacha <sacha.lesueur17@gmail.com>
2023-02-14 11:21:18 +0100

    YoloV8_tracking implementation

--- pipeline.py
+++ pipeline.py
@@ -14,6 +14,7 @@ parser = ArgumentParser()
 parser.add_argument('-r', '--ref', type=str)
 parser.add_argument('-i', '--image', type=str)
 parser.add_argument('-n', '--n-samples', type=int, default=3)
+parser.add_argument('-s', '--stride', type=int, default=3)
 
 TEMP_PFOLDER = '.temp/exp/crops/person/'
 
@@ -21,11 +22,11 @@ torch.no_grad()
 
 def main(opt):
     ref_img = cv.imread(opt['ref'], cv.IMREAD_ANYCOLOR)
-    ref_img = cv.resize(ref_img, (100,200))
-    #global_img = cv.imread(opt['image'], cv.IMREAD_ANYCOLOR)
+    ref_img = cv.cvtColor(ref_img, cv.COLOR_BGR2RGB)
+    ref_img = cv.resize(ref_img, (200,400))
 
     # YoloV8 detection
-    os.system(f'python ./yolov8_tracking/track.py --source {opt["image"]} --save-crop --project .temp --vid-stride 3')
+    os.system(f'python ./yolov8_tracking/track.py --source {opt["image"]} --save-crop --project .temp --vid-stride {opt["stride"]}')
 
     persons = os.listdir(TEMP_PFOLDER)
     print('Nb persons', len(persons))
@@ -34,27 +35,39 @@ def main(opt):
 
     imgs = []    
     for pfolder_path in persons:
-        pfolder = os.listdir(os.path.join(TEMP_PFOLDER ,pfolder_path))
+        pfolder = os.listdir(os.path.join(TEMP_PFOLDER, pfolder_path))
 
         n_samples = min(opt['n_samples'], len(pfolder))
+
         pimgs = random.choices(pfolder, k=n_samples)
-        pimgs = [cv.resize(cv.imread(os.path.join(TEMP_PFOLDER, pfolder_path, pth), cv.IMREAD_ANYCOLOR), (100,200)) for pth in pimgs]
-        imgs.append(pimgs)
+
+        img_batch = []
+        for pth in pimgs:
+            im = cv.imread(os.path.join(TEMP_PFOLDER, pfolder_path, pth), cv.IMREAD_ANYCOLOR)
+            im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
+            im = cv.resize(im, (200,400))
+            img_batch.append(im)
+                           
+        imgs.append(img_batch)
     
-    # Calcul average similarity for each person
+
+    # Similarity model
     model = SMResNet101()
     rn101 = nn.DataParallel(model)
     rn101 = rn101.to('cuda')
 
     rn101.load_state_dict(torch.load('./siamese_model/checkpoints-saved/checkpoint_rn101_400.pkl')['model'])
     
-    print(ref_img.shape)
     ref_preprocessed = np.expand_dims(ref_img, 0)
     ref_preprocessed = model.preprocessing(ref_preprocessed)
     ref_tensor = rn101(ref_preprocessed)
 
+    # Calcul average similarity for each person
     means = []
-    for img_batch in imgs:
+    min_val = 1000
+    min_idx = -1
+
+    for  batch_idx, img_batch in enumerate(imgs):
         img_batch = np.array(img_batch, dtype='float32')
         img_batch_preprocessed = model.preprocessing(img_batch)
         img_batch_tensors = rn101(img_batch_preprocessed)
@@ -62,13 +75,27 @@ def main(opt):
         distance_res = []
         for img_tensor in img_batch_tensors:
             distance_res.append(distance(ref_tensor,img_tensor).item())
+            if distance_res[-1] < min_val:
+                min_val = distance_res[-1]
+                min_idx = batch_idx
         
         distance_res = np.array(distance_res)
         means.append(np.average(distance_res))
 
-    print(means)
-    print('Folder ID:',np.argmin(means)+1)
+
+    idx = np.argmin(means)
+
+    print('--------------------')
+    print('Folder ID (mean):',idx+1)
+    print('Val (mean):','%0.2f'%means[idx])
+    print('Folder ID (min):', min_idx+1)
+    print('Val (min):','%0.2f'%min_val)
+
     shutil.rmtree('.temp')
+    cv.imwrite('result-mean.jpg', cv.cvtColor(imgs[idx][0], cv.COLOR_RGB2BGR))
+    cv.imwrite('result-min.jpg', cv.cvtColor(imgs[min_idx][0], cv.COLOR_RGB2BGR))
+
+
 
         
 
