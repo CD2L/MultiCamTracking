86e4da8836907540765f4a9664e3622958adf7a5
Sacha <sacha.lesueur17@gmail.com>
2023-02-17 14:10:32 +0100

    VGG16 in model.py

--- siamese_model/src/model.py
+++ siamese_model/src/model.py
@@ -3,6 +3,11 @@ from torch import nn
 from torchvision.models import resnet50, resnet101
 import numpy as np
 
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.models import Model
+
+
 class BaseModel(nn.Module):
     def __init__(self, parallele = False, partial_freeze = False):
         super().__init__()
@@ -12,6 +17,7 @@ class BaseModel(nn.Module):
     def preprocessing(self, x: np.ndarray):
         if len(x.shape) == 3:
             tensor = torch.from_numpy(x).permute(2,0,1)
+            tensor = tensor[None, :]
         else: 
             tensor = torch.from_numpy(x).permute(0,3,1,2)
         return tensor/255
@@ -29,7 +35,7 @@ class SiameseModel(BaseModel):
         if partial_freeze:
             for child in self.encoder.children():
                 ct += 1
-                if ct < 7:
+                if ct < 9:
                     for param in child.parameters():
                         param.requires_grad = False
 
@@ -50,4 +56,16 @@ class SMResNet101(BaseModel):
                         param.requires_grad = False
 
         if parallele:
-            self.encoder = nn.DataParallel(self.encoder)
+            self.encoder = nn.DataParallel(self.encoder)    
+
+class MCT_VGG16(BaseModel):
+    def __init__(self, parallele = False, partial_freeze = False) -> None:
+        self.encoder = VGG16()
+        self.encoder = Model(inputs=self.encoder.inputs, outputs=self.encoder.layers[-2].output)
+
+    def preprocessing(self, x: np.ndarray):
+        return preprocess_input(x)
+    
+    def __call__(self, x):
+        return self.encoder.predict(x, verbose=0)
+    
